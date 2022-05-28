cc09d6920580f990112a9b06e99ab26bcceac0a5
Adam <125adam123@gmail.com>
2022-05-28 13:24:25 +0200

    api route diagnosttics

--- pages/api/update-project.js
+++ pages/api/update-project.js
@@ -3,6 +3,9 @@ export default async (req, res) => {
     const {key} = req.query;
     if(key === process.env.UPDATE_KEY){
       const route = `/projects/${req.body?.entity?.attributes?.route}`;
+      console.log('---------------');
+      console.log(route);
+      console.log('---------------');
       try{
         await res.unstable_revalidate(route);
         res.status(200).json({
@@ -10,6 +13,7 @@ export default async (req, res) => {
         });
       }
       catch(e){
+        console.log(e);
         res.json({status: 'failed to reavildate'});
       }
     }
