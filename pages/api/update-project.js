28aa6fb6c72b99a21c0328cdfe3f0c982009e7a8
Adam <125adam123@gmail.com>
2022-05-28 18:45:35 +0200

    fixed url problems

--- pages/api/update-project.js
+++ pages/api/update-project.js
@@ -2,11 +2,11 @@
 export default async (req, res) => {
     const {key} = req.query;
     if(key === process.env.UPDATE_KEY){
-      const project_route = `projects/${req.body?.entity?.attributes?.route}`;
+      const project_route = `/projects/${req.body?.entity?.attributes?.route}`;
       try{
-        ['/','/pl/'].forEach(async locale => {
+        ['','/pl'].forEach(async locale => {
           await res.unstable_revalidate(`${locale}${project_route}`); //update changed project
-          await res.unstable_revalidate(locale); //update index
+          await res.unstable_revalidate(!locale ? '/' : locale); //update index
         })
         res.status(200).json({
           status: 'Data updated'
