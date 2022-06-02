d1950fba23e1a22ae49f93e311899185698aa6d6
Adam <125adam123@gmail.com>
2022-06-02 20:27:16 +0200

    changed gradients

--- src/consts/gradients.js
+++ src/consts/gradients.js
@@ -1,10 +1,12 @@
 export const gradients = [
-    `linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)`,
-    `linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)`,
-    `linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%)`,
     `linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%)`,
+    `linear-gradient(180deg, rgba(6,214,160,1) 0%, rgba(23,96,77,1) 100%)`,
+    `linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)`,
+    `linear-gradient(90deg, #FAD961 0%, #F76B1C 100%)`,
+    `linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)`,
     `linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)`,
     `linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)`,
     `linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)`,
     `linear-gradient(90deg, #FF9A8B 0%, #FF6A88 55%, #FF99AC 100%)`
 ]
+
